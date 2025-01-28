557fa1b9a90bb052b39e0a191c4d819f0f7b3d55
mostafasaad54969 <mostafasaad54969@gmail.com>
2025-01-28 04:48:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ print('Hello 123')
 classes = ["rip-seg"]
 DatasetPath = '/kaggle/working/RipCurrent/training_data/images'
 DestPath = '/kaggle/working/RipCurrent/10fold'
-AnnotationsPath = '/kaggle/working/RipCurrent/training_data/with_rips/labels'
+AnnotationsPath = '/kaggle/working/RipCurrent/training_data/labels'
 
 hf.kfold_split_yolo_dataset_yml(DestPath, DatasetPath, AnnotationsPath, classes)
