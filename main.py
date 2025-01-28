fc7e35690f1b98296b0fc170aef1a7b5daa42d35
mostafasaad54969 <mostafasaad54969@gmail.com>
2025-01-28 03:14:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import helper_functions as hf
 
 print('Hello 123')
 
-classes = ['rip-seg']
+classes = ["rip_current"]
 DatasetPath = '/kaggle/working/RipCurrent/training_data/images'
 DestPath = '/kaggle/working/RipCurrent/10fold'
 AnnotationsPath = '/kaggle/working/RipCurrent/training_data/labels'
