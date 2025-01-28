2c0470198cbf98bbaeddb44e214ca4f45cda7c43
mostafasaad54969 <mostafasaad54969@gmail.com>
2025-01-28 03:38:53 +0200

    Create VocToYolo.py

---
+++ VocToYolo.py
@@ -0,0 +1,5 @@
+import helper_functions as hf
+
+pascal_voc_to_yolo('/kaggle/working/RipCurrent/training_data/with_rips/images',
+                  '/kaggle/working/RipCurrent/training_data/with_rips/labels',
+                  '/kaggle/working/RipCurrent/training_data/labels', True)
